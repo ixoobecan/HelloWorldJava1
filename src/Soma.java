6435df49e863a9fb95dc970f64bb7ba2abf0be2c
IXOOBECAN <sixoobecan@gmail.com>
2022-10-09 20:42:49 -0300

    helloworldemjava

---
+++ src/Soma.java
@@ -0,0 +1,11 @@
+public class Soma {
+
+    public static int calcular(int n1, int n2) {
+        return n2 + n1;
+    }
+
+    public static void main(String[] args) {
+        int resultado = Soma.calcular(175, 5);
+        System.out.println(resultado);
+    }
+}
